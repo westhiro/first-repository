148ca4ef4a090a8d3537e8a8b58d0b5d961d45ab
westhiro <nishi-hiro0325@outlook.jp>
2022-11-02 12:42:30 +0900

    ブログ投稿一覧画面への導線追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -21,10 +21,10 @@ class PostController extends Controller
         return view('posts/create');
     }
     
-    public function store(Request $request, Post $post)
+    public function store(Post $post, PostRequest $request)
     {
-        $input = $request['post'];
+       $input = $request['post'];
         $post->fill($input)->save();
-        return redirect('/posts/'. $post->id);
+        return redirect('/posts/' . $post->id);
     }
 }
